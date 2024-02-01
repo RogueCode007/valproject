429fab0504efcb7c8e1658a8d2c098955c7e3918
RougeCode007 <abioyefeyifoluwa@gmail.com>
2024-02-01 16:48:08 +0100

    add files

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ThanksPage from "./components/ThanksPage.vue";
+import Question from "./components/Question.vue";
+import { ref } from "vue";
+
+const chosenYes = ref(false);
+</script>
+
+<template>
+  <Question v-if="!chosenYes" @choose-yes="chosenYes = true" />
+  <ThanksPage v-else />
+</template>
